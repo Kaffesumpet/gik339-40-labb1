3e68f33b96d851fe6a935b18bacb697c6ee7babd
Kronkan <h22thsta@du.se>
2023-11-21 11:20:59 +0100

    push push

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const element = document.querySelector("button");
+const checkboxElement = document.querySelector('input[type="checkbox"]');
+console.log(checkboxElement);
 
-element.textContent = "Uppgift 1";
+const textFields = document.querySelectorAll(".textfield");
+console.log(textFields);
 
-const textFields = document.querySelectorAll("input[type='text']");
-textFields.forEach(textField => {
-  textField.value = "Uppgift 2"; 
+// textFields.forEach(textField => {
+//   textField.value = "Uppgift 2"; 
+// });
+
+const btnElement = document.getElementById("myButton");
+console.log(btnElement);
+
+// btnElement.textContent = "Uppgift 1";
+
+const divElement = document.getElementsByClassName("divvar")[0];
+console.log(divElement);
+
+// divElement.style.backgroundColor = "red";
+
+
+checkboxElement.addEventListener("change", function() {
+  const colorInput = document.querySelector('input[name="color"]');
+  const colorValue = colorInput.value;
+  divElement.style.backgroundColor = colorValue;
 });
 
-const checkboxElement = document.getElementByID("divStyle");
-console.log(checkboxElement);
-TypeError: document.getElementByID is not a function
+function handleInput(e) {
+  console.log("Event triggered by: ", e.target);
+  const fieldName = e.target.name;
 
+  if (fieldName === "content") {
+    divElement.innerHTML = e.target.value;
+  }
+}
 
-const divElement = document.querySelector(".div")
-divElement.style.backgroundColor = "red";
+textFields.forEach(textField => {
+  textField.addEventListener("input", handleInput);
+});
+
+btnElement.addEventListener("click", function() {
+    divElement.remove();
+});
